17f5d9ea84538e93fa29e73dcd7309b8a3f0d732
seanrose <seanrose@alumni.stanford.edu>
2014-09-13 18:11:18 -0700

    edited color randomization

--- client/views/colors/colors.js
+++ client/views/colors/colors.js
@@ -1,25 +1,24 @@
 var colors = [
-	// ['#D0E78F', '#F6B759','#EC432E', '#584050', '#2D4C7A', '#9AD2E2'],
-	// ['#C9E8EB', '#54DCE8', '#2792D6', '#E0D2D2', '#254540', '#F3EBE8'],
+	['#D0E78F', '#F6B759','#EC432E', '#9AD2E2', '#584050', '#2D4C7A'],
+	['#C9E8EB', '#54DCE8', '#2792D6', '#E0D2D2', '#254540', '#F3EBE8'],
 	['#EEE0C6', '#F0A48A', '#6A917C', '#A4C9A7', '#CCE6CB', '#3E3634'],
-	// ['#E9E7EC', '#F8DEDD', '#FACC74', '#FD8C6A', '#D14747', '#B6D4A2'],
-	// ['#FDC680', '#D9654C', '#6195A0', '#C9C9C9', '#422F21', '#68275D'],
-	// ['#FFC67F', '#D9654C', '#68275D', '#422F21', '#6195A0', '#C9C9C9']
+	['#E9E7EC', '#F8DEDD', '#FACC74', '#FD8C6A', '#D14747', '#B6D4A2'],
+	['#FDC680', '#D9654C', '#6195A0', '#C9C9C9', '#422F21', '#68275D']
 ];
 
 function randomFrom(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function randomizeColor($template) {
+function randomizeColor() {
 	var randomColors = colors[randomFrom(0, colors.length - 1)];
 
-	$template('.color-space').each(function(i) {
-		$(this).css('background-color', randomColors[i]);
-		$(this).delay('slow').fadeIn(700 * i);
+	$('.color-space').each(function(i) {
+		var delay = 900;
+		$(this).css('background-color', randomColors[i]).delay('slow').fadeIn(delay * i);
 	});
 }
 
 Template.colors.rendered = function () {
-	randomizeColor(this.$);
+	randomizeColor();
 };
